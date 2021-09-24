979193476a76df5a5ae9901358c31f6a9fccb6bd
jansam123 <samueljankovych@gmail.com>
2021-09-24 10:57:32 +0200

    import change

--- spracovanie.py
+++ spracovanie.py
@@ -1,15 +1,8 @@
 import numpy as np
 import qexpy as q 
-import sys
+import praktika.Plot as pt
+import praktika.Table as tab
+import praktika.Data as dat
+import praktika.LoadData as ldat
 import pandas as pd 
-import matplotlib.pyplot as plt
-from matplotlib import container
-import os 
 
-
-
-#---------------ODSTRANENIE ERRORBAROV Z LEGENDY-------------------
-#handles, labels = ax.get_legend_handles_labels()
-#handles = [h[0] if isinstance(h, container.ErrorbarContainer) else h for h in handles]
-#ax.legend(handles, labels) 
-#------------------------------------------------------------------
